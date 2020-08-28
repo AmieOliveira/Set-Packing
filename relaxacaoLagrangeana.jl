298d6e883f0542fa5a377c019c5a6bd15dc84d1a
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2020-08-28 19:58:03 -0300

    Adicionadas funcoes 'limite_inferior' e 'check'. Acerto de bugs no codigo. NOTA: Nao esta verificado o funcionameto.

--- relaxacaoLagrangeana.jl
+++ relaxacaoLagrangeana.jl
@@ -31,20 +31,22 @@ function subproblema_lagrangeano(u)
     #return objective_value(ml), value.(x), s
 
     x = zeros(n)
-    cl_Max = -9999999
-    cl_MaxIdx = 0
-    for j in L
-        c_l = c[j] - sum(u[i] for i in P)
-        if c_l > cl_Max
-            cl_Max = c_l
-            cl_MaxIdx = j
-        end
+    cl_MaxIdx = 1
 
-        if c_l > 0 
+    c_l = zeros(n)
+
+    for j in L
+        c_l[j] = c[j] - sum(a[i,j]*u[i] for i in P)
+
+        if c_l[j] > 0 
             x[j] = 1
         end
+
+        if c_l[j] > c_l[cl_MaxIdx]
+            cl_MaxIdx = j
+        end
     end
-    if cl_Max < 0
+    if c_l[cl_MaxIdx] < 0
         x[cl_MaxIdx] = 1
     end
 
@@ -53,24 +55,80 @@ function subproblema_lagrangeano(u)
         s[i] = 1 - sum(a[i,j]*x[j] for j in L)
     end
 
-    z = sum(c[j]*x[j] for j in L) + sum( u[i]*( 1 - sum(a[i,j]*x[j] for j in L) ) for i in P)
-    # TODO: Reescrever de maneira menos ineficiente
+    # z = sum(c[j]*x[j] for j in L) + sum( u[i]*( 1 - sum(a[i,j]*x[j] for j in L) ) for i in P)
+    z = sum(c_l[j]*x[j] for j in P) + sum(u[i] for i in P)
 
     return z, x, s
 end
 
-function limite_inferior(u)
+function limite_inferior(custos, x_dual=[])
     # Retorna o z_low e o x_low
-    
-    # TODO
-    return 0, zeros(m)
+    x = zeros(m)
+
+    # Organizar elementos com relação aos pesos
+    idxs = sortperm(custos)
+    if ~isempty(x_dual)
+        tmpIn = []
+        tmpOut = []
+
+        for j in idxs
+            if x_dual[j] == 1
+                tmpIn = [tmpIn; j]
+            else
+                tmpOut = [tmpOut; j]
+            end
+        end
+
+        idxs = [tmpIn, tmpOut]
+    end
+
+    # Construir o limitante
+    restricoes = zeros(m)   # Uma restricao por produto. 
+    # Como x é um vetor nulo, ate agora todas tem valor zero (e, 
+    # consequentemente, nenhuma é violada)
+
+    for j in idxs
+        tmp = zeros(m)
+
+        viavel = true
+        for i in P
+            tmp[i] = a[i,j]
+            if restricoes[i] + tmp[i] > 1
+                viavel = false
+                break
+            end
+        end
+
+        if viavel
+            x[j] = 1
+            restricoes = restricoes + tmp
+        end
+    end
+
+    z = sum(c[j]*x[j] for j in L)
+    # Nao preciso calcular durante a escolha de x, porque a 
+    # adicao de um lance sempre aumenta o valor de z
+
+    # NOTE: Possivel implementar uma busca local aqui!
+
+    return z, x
 end
 
-function check(x_up)
+function check(x_up, u)
     # Retorna uma booleana que diz se x_up e otimo para o PEC
+    viavel = true
+    for i in P
+        if sum(a[i,j]*x_up[j] for j in L) > 1
+            viavel = false
+            break
+        end
+    end
 
-    # TODO
-    return false
+    z1 = sum(c[j]*x_up[j] for j in L)
+    z2 = sum(c[j]*x_up[j] for j in L) + sum( u[i]*( 1 - sum(a[i,j]*x_up[j] for j in L) ) for i in P)
+    compl = (z1 == z2) 
+
+    return viavel && compl
 end
 
 # ----------------------
@@ -86,44 +144,60 @@ function lagrangeana()
 
     eps = 0.1
 
-    z_low, x_low = limite_inferior(u)
+    z_low, x_low = limite_inferior(c)
 
     z_u_best = 9999999
+    x_u_best = zeros(n)
     k_best = 0
 
     z_l_best = z_low
-    x_best = zeros(m)
+    x_best = x_low
+
+    limInfType = "default"  # Pode ser "complementares" # NAO TESTADO
 
 
     while k < maxIter
         # Resolução do subproblema lagrangeano
         z_u, x_up, s = subproblema_lagrangeano(u)
+        #println("Limite dual: ", z_u, "(", x_up, ")")
 
         # Verificando se houve melhora
         if z_u < z_u_best
             z_u_best = z_u
+            x_u_best = x_up
             k_best = k
-            println("k_best", k_best)
+            println("k_best: ", k_best)
+            println("Novo z_up: ", z_u_best, " (", x_u_best, ")")
         end
         
         # Atualizando o limite limite_inferior
-        if k%trunc(Int64, maxIter/10) == 0
-            z_low, x_low = limite_inferior(u)
-            println("Atualizado z_low: ", z_low)
+        if (k%trunc(Int64, maxIter/10) == 0) && (k ≠ 0)
+            custo = zeros(n)
+            x_dual = []
+            if limInfType == "default"
+                for j in L
+                    custo[j] = c[j] - sum(a[i,j]*u[i] for i in P)
+                end
+            elseif limInfType == "complementares"
+                custo = c
+                x_dual = x_up
+            end
+            z_low, x_low = limite_inferior(custo, x_dual)
 
             # Verificando se houve melhora
             if z_low > z_l_best
                 z_l_best = z_low
                 x_best = x_low # NOTE: Prestar atencao, pq se x_up mudar x_best tb vai mudar
+                println("Atualizado z_low: ", z_l_best, " (", x_best, ")")
             end
         end
 
         # Condições de otimalidade
-        if z_u - z_low < 1
-            println("Parando por otimalidade (z == z_low)")
+        if z_u_best - z_l_best < 1
+            println("Parando por otimalidade (z_up == z_low)")
             break   # (z = z_low)
         end
-        if check(x_up)
+        if check(x_up, u)
             println("Parando por otimalidade (x_up é ótimo)")
             break
         end
@@ -142,10 +216,18 @@ function lagrangeana()
 
         # Atualizando o tamanho do passo e os multiplicadores de lagrange
         t = p_i*(z_u_best - z_l_best)/sum(s.^2)
-        u = u + t*(1+eps).*s
+        for j in L
+            u[j] = max(0, u[j] - t*(1+eps)*s[j]) # NOTE: Verificar!!! Se for a direcao oposta a de melhora mesmo, tem que ser menos
+        end
+
+        #println("Novos multiplicadores: ", u)
+        #println(s)
+        #println("")
 
         k = k + 1
     end
+
+    println("Iteração: ", k)
 
     return z_l_best, x_best
 end
